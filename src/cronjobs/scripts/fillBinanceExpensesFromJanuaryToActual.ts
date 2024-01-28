f3abb2bd5b71c36d3d9bc0112b026edb354ee115
racso2609 <oscaremiliolugo@gmail.com>
2024-01-28 14:04:23 -0400

    add cronjobs

---
+++ src/cronjobs/scripts/fillBinanceExpensesFromJanuaryToActual.ts
@@ -0,0 +1,14 @@
+import dotenv = require('dotenv');
+import { createExpense } from '@cronjobs/utils/binance';
+import { getMontshToUpdate } from '@cronjobs/utils/time';
+dotenv.config();
+
+(async () => {
+    const monthsToUpdate = getMontshToUpdate();
+    monthsToUpdate.forEach(({ startTimestamp, endTimestamp }) => {
+        const url = 'sapi/v1/c2c/orderMatch/listUserOrderHistory';
+        const query = `timestamp=${Date.now()}&tradeType=SELL&startTimestamp=${startTimestamp}&endTimestamp=${endTimestamp}`;
+
+        createExpense(url, query).then();
+    });
+})();
